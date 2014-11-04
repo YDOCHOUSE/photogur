73746362293ff46730d157af0241e5975c44f3d5
Cody House <codyahouse@gmail.com>
2014-11-04 18:33:20 -0500

    added database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,5 +19,28 @@ class PicturesController < ApplicationController
       }
     ]
   end
-  
+
+  def show
+  	@pictures = Picture.all 
+  end
+
+  def new
+
+  end
+
+  def edit
+  	
+  end
+
+  def create
+    	
+  end
+
+  def update
+  	
+  end
+
+  def destroy
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'pictures' => 'pictures#index'
-
+  # get 'pictures' => 'pictures#index'
+  resources :pictures 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
